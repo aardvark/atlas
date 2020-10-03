8b55f3f97e83d4e5deecc78036e5284ff203b41a
aardvark <e.a.latuhin@gmail.com>
2020-10-03 17:53:48 +0300

    Add in-place configuration for unpacked and mbin files

--- project.clj
+++ project.clj
@@ -24,7 +24,8 @@
                  [cljs-ajax "0.8.0"]
                  [org.clojure/data.xml "0.0.8"]
                  [com.github.kyleburton/clj-xpath "1.4.11"]
-                 [org.clojure/test.check "1.1.0"]]
+                 [org.clojure/test.check "1.1.0"]
+                 [com.grammarly/omniconf "0.4.2"]]
 
   :plugins [[lein-environ "1.1.0"]
             [lein-cljsbuild "1.1.7"]
--- src/clj/net/fiendishplatypus/nms/setup.clj
+++ src/clj/net/fiendishplatypus/nms/setup.clj
@@ -1,6 +1,19 @@
 (ns net.fiendishplatypus.nms.setup
   (:require [clojure.java.io]
-            [clojure.string]))
+            [clojure.java.shell]
+            [clojure.string]
+            [omniconf.core :as cfg]))
+
+;; Configuration 
+
+(cfg/define
+  {:lang-mbin-dir {:description "Directory where MBIN LANGUAGE files are located"
+                   :type :string}
+   :mbin-compiler-dir {:description "Directory where MBINCompiler.exe can be found"
+                       :type :string}})
+
+(cfg/populate-from-map {:lang-mbin-dir "D:\\NMSUnpacked\\LANGUAGE"
+                        :mbin-compiler-dir "C:\\Projects\\NoManSkyMods"})
 
 ;; File lookup operation
 
@@ -15,19 +28,13 @@
     (accept [this _ name]
       (engilsh-lang-file? name))))
 
-(comment 
-  (cfg/define
-    {:lang-mbin-dir {:description "Directory where MBIN LANGUAGE files are located"
-                     :type :string}})
-  
-  (cfg/get :lang-mbin-dir))
 
 (defn list-lang-files
   ([path-to-lang-dir]
    (.listFiles (java.io.File. path-to-lang-dir)
                engilsh-lang-file))
   ([]
-   (list-lang-files "D:\\NMSUnpacked\\LANGUAGE")))
+   (list-lang-files (cfg/get :lang-mbin-dir))))
 
 ;; File checksum operations
 
@@ -40,35 +47,54 @@
                      (.digest (.getMessageDigest dis)))]
    digest-arr))
 
+;; `file-and-digest` function can be used to do a smart conversion.
+;; Meaning we can only run conversion to XML if new file is different
+;; from previous one.
+;; This raise a separate question of storing 'conversion-run'
+;; results somewhere and loading them for checking on the next run
+;; saving map of :filename and :sha256sum should be enough
+;; :mbin-file and :exml-file parameters can be always added as they
+;; appear in the processing step
 
 (defn file-and-digest
   [file]
   (let [digest-arr (file-to-a-digest file)]
     {:filename (.getName file) 
+     :mbin-file file
      :sha256sum (clojure.string/join (map #(format "%02x" %)
                                           (seq digest-arr)))}))
 
 (comment
   (map file-and-digest (list-lang-files)))
 
-;; copy file?
+
+(defn file-to-name-ext-pair 
+  [filename]
+  (let [[name ext] (clojure.string/split filename #"\.")]
+    {:name name :ext ext}))
 
 ;; MBIN decompiler invocation
+
 (defn mbin->exml 
-  [mbin]
-  (let [mbin-compiler-dir "C:\\Projects\\NoManSkyMods"
-        filename "NMS_LOC1_ENGLISH.MBIN"]
+  "Convert MBIN `file` to the EXML, and return it. 
+   Converted file will be located in same directory as a passed MBIN `file`. "
+  [file]
+  (let [mbin-compiler-dir (cfg/get :mbin-compiler-dir)
+        path-str (.toString (.toPath file))
+        filename (.getName file)
+        parent-dir (.getParent file)
+        exml-file-path (str parent-dir
+                                 "\\"
+                                 (:name (file-to-name-ext-pair filename))
+                                 ".EXML")]
+
     (clojure.java.shell/with-sh-dir mbin-compiler-dir
-      (clojure.java.shell/sh "cmd.exe" "/C" "call" "MBINCompiler.exe" "-q" filename))
-    (.length (java.io.File. "C:\\Projects\\NoManSkyMods\\NMS_LOC1_ENGLISH.EXML"))))
+      (clojure.java.shell/sh "cmd.exe" "/C" "call" "MBINCompiler.exe" "-q" path-str))
+    (java.io.File. exml-file-path)))
 
 (comment
-  (mbin->exml "whatever"))
+  (mbin->exml (first (list-lang-files))))
 
-(comment 
-  "true"
-  (engilsh-lang-file? "NMS_LOC1_ENGLISH.MBIN")
-  "false"
-  (engilsh-lang-file? "NMS_LOC1_GERMAN.MBIN"))
-  
-
+(defn language-files 
+  []
+  (map mbin->exml (list-lang-files)))

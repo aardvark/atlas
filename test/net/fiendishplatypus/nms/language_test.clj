a8dc03f87079e5a186224a7f77b78183419f6080
aardvark <aardvark@bk.ru>
2021-05-02 14:14:42 +0300

    Move languate tests to correct ns

--- test/net/fiendishplatypus/nms/language_test.clj
+++ test/net/fiendishplatypus/nms/language_test.clj
@@ -1,2 +1,17 @@
 (ns net.fiendishplatypus.nms.language-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [net.fiendishplatypus.nms.language :as language]))
+
+
+(comment
+  (language/language
+   (slurp "test/resources/language-with-quote-in-id.xml")
+   "EXP_LIFEFORM'S"))
+
+(deftest language-lookup
+  (testing "language/language lookup"
+    (is (= {"EXP_LIFEFORM'S" "lifeform's"}
+           (language/language
+            (slurp "test/resources/language-with-quote-in-id.xml")
+            "EXP_LIFEFORM'S"))
+        "Lookup of EXP_LIFEFORM'S id should return \"lifeform's\" value")))
